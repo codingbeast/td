bbff93b04d13b9b6258b8cf637414f7996330e77
Raaj <raj0kumar00@gmail.com>
2025-05-02 07:45:12 +0530

    cpse data

---
+++ td/config/strategies/cpse.py
@@ -0,0 +1,20 @@
+from jugaad_trader import Zerodha
+
+GOLDBEES_CONFIG = {
+    "ticker": "CPSEETF",
+    "stock": "CPSEETF",
+    "exchange": Zerodha.EXCHANGE_NSE,
+    "variety": Zerodha.VARIETY_AMO,
+    "order_type": Zerodha.ORDER_TYPE_LIMIT,
+    "product_type": Zerodha.PRODUCT_CNC,
+    "min_sell_qnt": 2,
+    "uptrand_price": 8000,
+    "downtrand_price": 10000,
+    "profit_percentage": 1.03,
+    "run_before_time": "09:00:00",
+    "run_after_time": "15:00:00",
+    "run_on_days": "0,1,2,3,4",
+    "is_buy": True,
+    "amount_strict": False,
+    "enabled": True
+}
